c8a170023a6f643fb79cc4e7a8ffcd55eace0e7a
Cooperator-gem <97656070+Cooperator-gem@users.noreply.github.com>
2022-02-04 15:36:26 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -41,7 +41,7 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+            InlineKeyboardButton('🔰OTT MOVIES🔰', url='https://t.me/+S384rYHJgf1mYmQ1')
             ],[
             InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
             InlineKeyboardButton('⭕GROUP⭕', url='https://t.me/joinchat/-3Vjw61udNM3OWU9')
@@ -82,7 +82,7 @@ async def start(client, message):
         return
     if len(message.command) ==2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[
-            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+            InlineKeyboardButton('🔰OTT MOVIES🔰', url='https://t.me/+S384rYHJgf1mYmQ1')
             ],[
             InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
             InlineKeyboardButton('⭕GROUP⭕', url='https://t.me/joinchat/-3Vjw61udNM3OWU9')
